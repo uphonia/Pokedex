c20bae0d62c0f2df05c6e93f96ff07aa36e52ddc
uphonia <wu.angela0@gmail.com>
2021-06-20 20:28:06 -0400

    Updates

---
+++ precache-manifest.16c0e9e1632648c3f8bc28be82530fb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b2c3293235fdea59ee4",
+    "url": "/uphonia/Pokepedia/static/css/main.c1b39e53.chunk.css"
+  },
+  {
+    "revision": "2b2c3293235fdea59ee4",
+    "url": "/uphonia/Pokepedia/static/js/main.d39908a5.chunk.js"
+  },
+  {
+    "revision": "eb62d0543a50b2ecf1ca",
+    "url": "/uphonia/Pokepedia/static/js/runtime~main.1a09b4fa.js"
+  },
+  {
+    "revision": "c8df4f7455fb1bc7c758",
+    "url": "/uphonia/Pokepedia/static/js/2.1f03838c.chunk.js"
+  },
+  {
+    "revision": "db2b02417f71374ccf9cbd4535ba1354",
+    "url": "/uphonia/Pokepedia/index.html"
+  }
+];
